7e7ddf2f687051c2e56a0d6e4ceecc6ab10397ca
wdharmawan <wisnudharmawan844@gmail.com>
2022-12-05 13:47:31 +0700

    ocr1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import Tesseract from 'tesseract.js';
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [text, setText] = useState("");
+  const [image, setImage] = useState("");
+  const [progress, setProgress] = useState(0);
+  
+  const handleClick = ()=> {
+    setIsLoading(true);
+    Tesseract.recognize(image,"eng",{ 
+      logger: m => {
+        console.log(m); 
+        if (m.status  === "recognizing text") {
+          setProgress( parseInt(m.progress * 100) )
+        }
+      }, 
+    }).then(({ data: { text } }) => {
+      setText(text);
+      setIsLoading(false);
+    });
+  }; 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" style={{height:"100vh"}}>
+      <div className="row h-100">
+        <div className="col-md-5 mx-auto d-flex flex-column align-items-center">
+          {!isLoading && <h1 className="mt-5 mb-4 pb-5">gambar ke text</h1>}
+
+          {/* form */}
+          {
+            !isLoading && !text && (
+              <>
+                <input type="file" className="form-control mt-5" onChange={(e)=> setImage(URL.createObjectURL(e.target.files[0]))
+                }/>
+                <input type="button" className="form-control btn btn-primary mt-4" value="Convert" onClick={handleClick}  />
+              </>
+            )
+          }
+          {/* progress bar */}
+          {
+            isLoading && (
+              <>
+                <p className="text-center mt-5">Converting : {progress}%</p>
+              </>
+            )
+          }
+          {/* text area */}
+          {
+            !isLoading && text && (
+                <textarea className="form-control"  rows="20" value={text} onChange={(e) => setText(e.target.value)}></textarea>
+            )
+          }
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
